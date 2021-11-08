58fb94699f771a10513c312f219f17429b5e333a
Leandro Cadena <63469188+LeandroCadena@users.noreply.github.com>
2021-11-07 21:00:21 -0300

    Delete api directory

--- api/src/routes/checkout.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { checkoutController } from '../controllers/checkout.controller'
-
-const router = Router();
-
-router.get('/:id', checkoutController);
-
-export default router;
--- api/src/routes/checkout_MP.routes.ts
+++
@@ -1,8 +0,0 @@
-// import { Router } from "express";
-// import { postPayment } from "../controllers/checkout_MP";
-
-// const router = Router();
-
-// router.post("/", postPayment);
-
-// export default router;
